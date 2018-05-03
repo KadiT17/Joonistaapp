cac4f25050008b979ebe861dcc713063777eed9e
Opilane TTHK <opilane@tthk.ee>
2018-05-03 10:00:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/opilane/joonistaapp/DrawingView.java
@@ -0,0 +1,19 @@
+package com.example.opilane.joonistaapp;
+
+import android.content.Context;
+import android.view.View;
+
+import java.util.jar.Attributes;
+
+
+public class DrawingView extends View {
+
+
+
+    public DrawingView(Context context, Attributeset attra) {
+        super(context);
+    }
+       private void setupDrawing () {
+
+    }
+}
